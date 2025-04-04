8ac9ce401cd036dc8254b6ef90ccc182b8cb8b23
Shaffan <shaffan1996@gmail.com>
2025-04-04 16:20:59 -0400

    finished 05-vec

--- exercises/02_functions/functions4.rs
+++ exercises/02_functions/functions4.rs
@@ -1,6 +1,9 @@
-// This store is having a sale where if the price is an even number, you get 10
-// Rustbucks off, but if it's an odd number, it's 3 Rustbucks off.
-// Don't worry about the function bodies themselves, we are only interested in
+// This store is having a sale where if the price is an
+// even number, you get 10
+// Rustbucks off, but if it's an odd number,
+// it's 3 Rustbucks off.
+// Don't worry about the function bodies themselves,
+// we are only interested in
 // the signatures for now.
 
 fn is_even(num: i64) -> bool {
@@ -8,7 +11,7 @@ fn is_even(num: i64) -> bool {
 }
 
 // TODO: Fix the function signature.
-fn sale_price(price: i64) -> {
+fn sale_price(price: i64) -> i64 {
     if is_even(price) {
         price - 10
     } else {
